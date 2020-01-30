0ced7afa47b4dfffa24a36c06dfd37bad2a62101
den25570 <den255700@gmail.com>
2020-01-30 13:45:34 +0300

    product_page_init

---
+++ assets/js/product_tab_script.js
@@ -0,0 +1,20 @@
+
+function open_product_tab(evt, cityName) {
+    let i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("product_info__tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("product_info__tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
