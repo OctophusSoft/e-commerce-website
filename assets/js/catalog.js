84d38e0cadaf4d5882cd2cc2932d084c3c4c5f30
German <GermanletchiK@yandex.ru>
2020-01-31 21:22:01 +0300

    'catalog-card'

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -9,4 +9,14 @@ $(document).ready(function() {
 			$(this).parent().find('.catalog_page__popup__body__item__body').addClass('popup_active');
 		}
 	});
+	
+	$('.catalog__body__card__main').hover(function() {
+		$(this).find('.catalog__body__card__hover_panel').animate({'opacity': '1'}, 500);
+		$(this).find('.catalog__body__card__stars').animate({'opacity': '0'}, 500);
+	});
+	
+	$('.catalog__body__card__main').mouseleave(function() {
+		$(this).find('.catalog__body__card__hover_panel').animate({'opacity': '0'}, 200);
+		$(this).find('.catalog__body__card__stars').animate({'opacity': '1'}, 200);
+	});
 });
